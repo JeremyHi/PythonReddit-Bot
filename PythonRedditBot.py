45bb216e2f5d84afed8fe706d06c92a7ec7804f4
Jeremy <jhitchco@lion.lmu.edu>
2016-12-28 00:41:13 -0800

    working for all except picture posts. Also need to get timing down.

--- PythonRedditBot.py
+++ PythonRedditBot.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import praw, tweepy, time, sys
+from pyshorteners import Shortener
 
-tokenFile = "TwitterInfo.txt" #file containing OAuth infomation, 1 line at a time
+tokenFile = "TwitterInfo.txt" #file containing OAuth infomation
 filename = open(tokenFile,'r')
 f = filename.readlines()
 filename.close()
-keys = []
+keys = [] #keys in order for use below
 
 for line in f:
 	keys.append(line[:-1])
@@ -17,19 +14,20 @@ CONSUMER_KEY = keys[0]
 CONSUMER_SECRET = keys[1]
 ACCESS_KEY = keys[2]
 ACCESS_SECRET = keys[3]
+api_key = keys[4]
+shortener = Shortener('Google', api_key=api_key)	
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 reddit = praw.Reddit('bot1')
-netsec = reddit.subreddit("netsec")
-data = {}
+subreddit = reddit.front
+data = {} #Contains the post data
 
-key = 1
-for submission in netsec.new(limit=1):
-	data["Title" + str(key)] = str(submission.title)
-	data["URL" + str(key)] = str(submission.url)
-	data["Author" + str(key)] = "/u/" + str(submission.author)
-	#key+=1
-
-print(data)
+for submission in subreddit.hot(limit=1):
+	data["Title"] = str(submission.title)
+	data["Author"] = "/u/" + str(submission.author)
+	data["URL"] = str(submission.url)
+	api.update_status(data["Title"] + "\n" + "Subreddit: " + 
+		str(subreddit) + "\n" + str(shortener.short(data["URL"])))
+	time.sleep(12)

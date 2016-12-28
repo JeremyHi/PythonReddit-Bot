9cda8901a0637c416c9c5918ad524b371ce36836
Jeremy <jhitchco@lion.lmu.edu>
2016-12-27 23:18:29 -0800

    added Twitter Information

--- PythonRedditBot.py
+++ PythonRedditBot.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python
-import praw
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import praw, tweepy, time, sys
+
+tokenFile = "TwitterInfo.txt" #file containing OAuth infomation, 1 line at a time
+filename = open(tokenFile,'r')
+f = filename.readlines()
+filename.close()
+keys = []
+
+for line in f:
+	keys.append(line[:-1])
+
+CONSUMER_KEY = keys[0]
+CONSUMER_SECRET = keys[1]
+ACCESS_KEY = keys[2]
+ACCESS_SECRET = keys[3]
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
 
 reddit = praw.Reddit('bot1')
 netsec = reddit.subreddit("netsec")
